15067fd7c9f1abddbd5ef1de7a9f8373ac88a1a3
cyrille <cyrilleotieno7@gmail.com>
2021-02-13 06:37:09 +0300

    checkbet for null

--- php_handlers/checkbet.php
+++ php_handlers/checkbet.php
@@ -20,7 +20,7 @@ while ($row = $stmt->fetch()) {
         if ($row2->result == NULL) {
             echo ("this is null</br>");
             $status3 = 2;
-            break;
+            $status=0;
         } else if ($row2->bet_value == 1 & $row2->result == "home" || $row2->bet_value == 2 & $row2->result == "draw" || $row2->bet_value == 3 & $row2->result == "away") {
             echo ("one correct</br>");
             $status = 1;
@@ -31,7 +31,10 @@ while ($row = $stmt->fetch()) {
         }
     }
     echo (" one bet done</br>");
-    if ($status == 1) {
+    if ($status3 == 2 || $status==0) {
+        echo ("this bet is still pending" . $id . "</br>");
+    }
+    else if ($status == 1) {
         echo ("this bet has won" . $id . "</br>");
         $sql3 = "UPDATE bets_table set bet_status=? where bet_id=?";
         $stmt3 = $conn->prepare($sql3);
@@ -73,7 +76,5 @@ while ($row = $stmt->fetch()) {
         $sql3 = "UPDATE bets_table set bet_status=? where bet_id=?";
         $stmt3 = $conn->prepare($sql3);
         $stmt3->execute(array("Lost", $id));
-    } elseif ($status3 == 2) {
-        echo ("this bet is still pending" . $id . "</br>");
-    }
+    } 
 }
