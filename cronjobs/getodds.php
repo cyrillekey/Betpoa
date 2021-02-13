e81d55bf8bd3db596e82e1bde75f032aaa797eef
cyrille <cyrilleotieno7@gmail.com>
2021-02-13 17:06:48 +0300

    get odd

--- cronjobs/getodds.php
+++ cronjobs/getodds.php
@@ -1,10 +1,10 @@
 <?php
 require('conn/conn.php');
-$leagues=["2790","2794","2796","2803","2833","2857","2664","2755","2771","2777"];
+$leagues=["2790"/*,"2794","2796","2803","2833","2857","2664","2755","2771","2777"*/];
 $curl = curl_init();
 foreach ($leagues as $key => $league) {
     $p=0;
-while($p<10){
+while($p<6){
 curl_setopt_array($curl, [
 	CURLOPT_URL => "https://api-football-v1.p.rapidapi.com/v2/odds/league/".$league."?page=".$p."",
 	CURLOPT_RETURNTRANSFER => true,
