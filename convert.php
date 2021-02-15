e63807a7019cb37469603135f73c31614fabf1a5
cyrille <cyrilleotieno7@gmail.com>
2021-02-15 10:40:01 +0300

    conver.php

--- convert.php
+++ convert.php
@@ -2,11 +2,11 @@
 require('conn/conn.php');
 $leagues=["2790","2794","2796","2803","2833","2857","2664","2755","2771","2777"];
 $curl = curl_init();
-
+foreach ($leagues as $key => $league) {
     $p=1;
 while($p<6){
 curl_setopt_array($curl, [
-	CURLOPT_URL => "https://api-football-v1.p.rapidapi.com/v2/odds/league/2777?page=".$p,
+	CURLOPT_URL => "https://api-football-v1.p.rapidapi.com/v2/odds/league/".$league."?page=".$p,
 	CURLOPT_RETURNTRANSFER => true,
 	CURLOPT_FOLLOWLOCATION => true,
 	CURLOPT_ENCODING => "",
@@ -51,4 +51,5 @@ echo" one worked";
     $x++;
 }}
 $p++;
+}
 }
