803337031334dca655b61735d6ca464bbcf30f2f
cyrille <cyrilleotieno7@gmail.com>
2021-02-25 21:15:32 +0300

    hello

--- index.php
+++ index.php
@@ -241,7 +241,7 @@ form.example::after {
     // and in line with other parts of your application
 
     $current_time = time();
-    $sql = "SELECT * from game_odds where commence_time > ? and gamestatus= ?  ORDER BY commence_time ASC";
+    $sql = "SELECT * from game_odds where commence_time > ? and gamestatus= ? ORDER BY commence_time ASC";
     $stmt = $conn->prepare($sql);
     $stmt->execute([$current_time,"NS"]);
     while ($row = $stmt->fetch()) {
