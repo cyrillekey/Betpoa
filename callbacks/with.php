e358c4d2a31b3fc38eb4560b6264c17ad0922434
cyrille <cyrilleotieno7@gmail.com>
2021-03-30 16:48:49 +0300

    help

--- callbacks/with.php
+++ callbacks/with.php
@@ -1,3 +1,9 @@
 <?php
 $callbackResponse = file_get_contents('php://input');
-	printf($callbackResponse);
+$response=json_decode($callbackResponse,true);
+$id=($response['Result']['ResultParameters']['ResultParameter'][0]['Value']);
+$amount=($response['Result']['ResultParameters']['ResultParameter'][1]['Value']);
+$time=strtotime($date);
+$date=($response['Result']['ResultParameters']['ResultParameter'][4]['Value']);
+$time=strtotime($date);
+echo("id $id amount $amount and date $time" );
--- index.php
+++ index.php
@@ -283,7 +283,7 @@ $stmt->execute([$current_time,"NS"]);
                         ' . $row->away_win . '
                         </div>
                         </a>
-                        <a class="more-markets " href="#">+0</a>
+                        <a class="more-markets " href="html/markets.php?game='.$row->fixture_id.'">+30</a>
                     </div>
                 </div>
             </div>
