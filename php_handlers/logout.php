2b21be1bff15dffc613c97d12848946d7b3a3743
cyrille <cyrilleotieno7@gmail.com>
2021-04-12 13:29:12 +0300

    clear cookie on logout

--- php_handlers/logout.php
+++ php_handlers/logout.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
 unset($_SESSION['usernumber']);
+if(isset($_COOKIE['remember'])){
+    unset($_COOKIE['remember']);
+    setcookie('remember', null, -1, '/');
+}
 header('location:../html/login.php');
 exit();
