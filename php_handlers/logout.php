f0a85d68d5c50b5f361857a5e1ce2ba5f7c18ef2
cyrille <cyrilleotieno7@gmail.com>
2021-04-12 13:34:07 +0300

    logout should wor

--- php_handlers/logout.php
+++ php_handlers/logout.php
@@ -3,7 +3,7 @@ session_start();
 unset($_SESSION['usernumber']);
 if(isset($_COOKIE['remember'])){
     unset($_COOKIE['remember']);
-    setcookie('remember', null, -1, '/');
+    setcookie('remember', " ", time()-3600, '/');
 }
 header('location:../html/login.php');
 exit();
