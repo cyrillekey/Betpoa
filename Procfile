2fa4e9c25fc045694b9278084efbeeac0b3974e1
cyrille <cyrilleotieno7@gmail.com>
2021-04-15 13:50:16 +0300

    wow

--- Procfile
+++ Procfile
@@ -3,3 +3,4 @@ worker: php cronjobs/results.php
 processor: php cronjobs/getodds.php
 runner: php php_handlers/checkbet.php 
 getter: php cronjobs/fixtures.php
+league: php cronjobs/how.php
---
+++ cronjobs/how.php
@@ -0,0 +1,40 @@
+<?php
+include('conn/conn.php');
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+	CURLOPT_URL => "https://api-football-v1.p.rapidapi.com/v2/leagues/season/2020",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"x-rapidapi-host: api-football-v1.p.rapidapi.com",
+		"x-rapidapi-key: e56261b2e2msha48fb697c1e185dp18d4ffjsn3656a9b1fd85"
+	],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+    $x=0;
+    while ($x <= 10) {
+        # code...
+    
+	$result=json_decode($response);
+    $id=$result->api->leagues[$x]->league_id;
+    $name=$result->api->leagues[$x]->name;
+    $sql="INSERT into league_table VALUES(:id,:named);";
+    $stmt=$conn->prepare($sql);
+    $stmt->execute([$id,$name]);
+    $x++;
+}
+}
