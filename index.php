b0cb1afab01918972fdbdf8a60ff9268d599cefa
cyrille <cyrilleotieno7@gmail.com>
2021-05-14 19:04:02 +0300

    mondat

--- index.php
+++ index.php
@@ -141,12 +141,13 @@ form.example::after {
                         <a href="html/mybets.php" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">My bets</a>
                         <a href="html/deposit.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Deposit</a>
                         <a href="html/withdraw.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Withdraw</a>
-                        
-                    </div>
+                        ';
+                        if (isset($_SESSION['usernumber'])=="0708073370") {
+                            echo'<a href="html/dashboard.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>';
+                        }
+                    echo'</div>
                 </div>';
-            if (isset($_SESSION['usernumber'])=="0708073370") {
-                echo'<a href="html/dashboard.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>';
-            }
+            
                 
                 } else {
                     echo '
